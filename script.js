ff7e6a34bdc5bb338ed41831bb6ff2db34e5af9e
John Cross <john.william.cross@gmail.com>
2020-09-03 14:07:04 -0700

    add js to edit page, begin landing page

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 $("#save-card").click(function () {
-    // code below targets the id "overlay-sucess"; for toggleClass, 
-    // it means "if d-flex is on, turn it off; if d-none is on, turn it off"
-    $("#overlay-success").toggleClass("d-flex d-none")
-})
+   // code below targets the id "overlay-sucess"; for toggleClass,
+   // it means "if d-flex is on, turn it off; if d-none is on, turn it off"
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
 
 $("#back-to-answer-error").click(function () {
-    $("#overlay-error").toggleClass("d-flex d-none")
-})
+   $("#overlay-error").toggleClass("d-flex d-none");
+});
 
 $("#next-error").click(function () {
-    $("#overlay-error").toggleClass("d-flex d-none")
-})
+   $("#overlay-error").toggleClass("d-flex d-none");
+});
+
+$("#show-delete-button").click(function () {
+   $("#delete-this-card").toggleClass("d-none");
+});
