289a0bd151bf5636f3e3654f086ade0abcbf3c68
John Cross <john.william.cross@gmail.com>
2020-09-15 14:23:28 -0700

    add some working char counts

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ $(".sign-up-prompt").click(function () {
    $(".show-sign-up-info").toggleClass("d-none");
    $(".email-and-create-password").toggleClass("d-none");
 });
+
+let createImageryInputCharsCount = 0; // use "let" instead of "const" because it will be updated
+$("#create-imagery-input").keypress(function () {
+   console.log("Let's add 1!"); // double quotes surround a string!
+   /*
+   ---
+   We're about to work with numbers in JavaScript!
+   ---
+   */
+   createImageryInputCharsCount++; // += 1 increments by one; ++ also works (as does --)"let" is another way; "const" is another
+   console.log("Total inputted chars: ", createImageryInputCharsCount);
+   $("#imagery-char-count").html(createImageryInputCharsCount);
+});
+
+let editBottomCardInputCharsCount = 0;
+$("#edit-card-char-count-bottom-of-card").keypress(function () {
+   editBottomCardInputCharsCount++;
+   console.log(
+      "Total inputted chars bottom card:  ",
+      editBottomCardInputCharsCount
+   );
+   $("#edit-bottom-card-char-count").html(editBottomCardInputCharsCount);
+});
+
+let editTopCardInputCharsCount = 0;
+$("#edit-card-char-count-top-of-card").keypress(function () {
+   editTopCardInputCharsCount++;
+   console.log("Total inputted chars top card: ", editTopCardInputCharsCount);
+   $("#edit-top-card-char-count").html(editTopCardInputCharsCount);
+});
+
+let createAnswerCardInputCharsCount = 0;
+$("#create-answer-input").keypress(function () {
+   createAnswerCardInputCharsCount++;
+   console.log(
+      "Total inputted chars Create Answer card: ",
+      createAnswerCardInputCharsCount
+   );
+   $("#create-answer-char-count").html(createAnswerCardInputCharsCount);
+});
