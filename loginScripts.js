da6b6553f2ee95d4f9593aa6d42a51e872caba74
John Cross <john.william.cross@gmail.com>
2020-09-24 11:11:00 -0700

    truely begin to refactor my code now. or at least attempt to. I just finished the video about functions and side effects. so we'll see...

--- loginScripts.js
+++ loginScripts.js
@@ -1,38 +1,38 @@
-//fake database
-const dbUserbDetails = [`mike@gmail.com`, `javascriptiscool`];
+// //fake database
+// const dbUserbDetails = [`mike@gmail.com`, `javascriptiscool`];
 
-$(`#login-button`).click(function () {
-   // this console.log is just to show that we have access to vars/all scripts inside the script.js file
-   //    console.log(
-   //       `here's the max char var from loginscripts:  ${maxCharCardInput}`
-   //    );
+// $(`#login-button`).click(function () {
+//    // this console.log is just to show that we have access to vars/all scripts inside the script.js file
+//    //    console.log(
+//    //       `here's the max char var from loginscripts:  ${maxCharCardInput}`
+//    //    );
 
-   const emailInput = $(`#return-user-email`).val();
-   const passwordInput = $(`#return-user-password`).val();
-   const userInputDetails = [emailInput, passwordInput];
+//    const emailInput = $(`#return-user-email`).val();
+//    const passwordInput = $(`#return-user-password`).val();
+//    const userInputDetails = [emailInput, passwordInput];
 
-   const isUserInDb = checkIsUserInDb(dbUserbDetails, userInputDetails);
-   if (isUserInDb) {
-      //contine
-      console.log(`Let's continue`);
-      disableElement(`#login-button`);
-      disableElement(`#return-user-password`);
-   } else {
-      console.log(`This user is not found.`);
-   }
-});
+//    const isUserInDb = checkIsUserInDb(dbUserbDetails, userInputDetails);
+//    if (isUserInDb) {
+//       //contine
+//       console.log(`Let's continue`);
+//       disableElement(`#login-button`);
+//       disableElement(`#return-user-password`);
+//    } else {
+//       console.log(`This user is not found.`);
+//    }
+// });
 
-function disableElement(id) {
-   $(id).attr(`disabled`, `disabled`); //side effect that happens outside the function
-   //we use it when we want something to happen inside the application
-}
+// function disableElement(id) {
+//    $(id).attr(`disabled`, `disabled`); //side effect that happens outside the function
+//    //we use it when we want something to happen inside the application
+// }
 
-function checkIsUserInDb(dbUserbDetails, userInputDetails) {
-   //this is a pure function
-   if (
-      dbUserbDetails[0] === userInputDetails[0] &&
-      dbUserbDetails[1] === userInputDetails[1]
-   ) {
-      return true;
-   } else false;
-}
+// function checkIsUserInDb(dbUserbDetails, userInputDetails) {
+//    //this is a pure function
+//    if (
+//       dbUserbDetails[0] === userInputDetails[0] &&
+//       dbUserbDetails[1] === userInputDetails[1]
+//    ) {
+//       return true;
+//    } else false;
+// }
