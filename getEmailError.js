28f9ab16c1fa436f2f4b8b0227db3e8f13eacca0
John Cross <john.william.cross@gmail.com>
2020-09-24 06:36:06 -0700

    begin refactoring before any major changes

---
+++ getEmailError.js
@@ -0,0 +1,6 @@
+function getEmailError(emailInput) {
+   // check the user email input against all validation.
+   // if emailInput is blank
+   // return `Please enter your email address.`
+   // else return `` // an empty string
+}
