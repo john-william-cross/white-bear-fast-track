6b81d9f31a3479c5889acd54a5d635092aae464b
John Cross <john.william.cross@gmail.com>
2020-09-24 07:10:49 -0700

    start notes on new functions video

--- getEmailError.js
+++ getEmailError.js
@@ -1,29 +1,29 @@
-function getEmailError(emailInput) {
-   const newUserEmailAddress = $(`#new-user-email`).val();
-   const newUserEmailAddressLength = newUserEmailAddress.length;
-   const trimmedNewUserEmailAddress = newUserEmailAddress.trim();
-   const partsOfTrimmedNewUserEmail = trimmedNewUserEmailAddress.split(`@`);
-   const localPartTrimmedNewUserEmail = partsOfTrimmedNewUserEmail[0];
+// function getEmailError(emailInput) {
+//    const newUserEmailAddress = $(`#new-user-email`).val();
+//    const newUserEmailAddressLength = newUserEmailAddress.length;
+//    const trimmedNewUserEmailAddress = newUserEmailAddress.trim();
+//    const partsOfTrimmedNewUserEmail = trimmedNewUserEmailAddress.split(`@`);
+//    const localPartTrimmedNewUserEmail = partsOfTrimmedNewUserEmail[0];
 
-   if (newUserEmailAddressLength === 0) {
-      $(`#email-error-message`).removeClass(`d-none`);
-      $(`#new-user-email`).addClass(`is-invalid`);
-   } else if (newUserEmailAddressLength > 0) {
-      $(`#email-error-message`).addClass(`d-none`);
-      $(`#new-user-email`).removeClass(`is-invalid`);
-   }
-}
+//    if (newUserEmailAddressLength === 0) {
+//       $(`#email-error-message`).removeClass(`d-none`);
+//       $(`#new-user-email`).addClass(`is-invalid`);
+//    } else if (newUserEmailAddressLength > 0) {
+//       $(`#email-error-message`).addClass(`d-none`);
+//       $(`#new-user-email`).removeClass(`is-invalid`);
+//    }
+// }
 
-// check the user email input against all validation.
-// if emailInput is blank
-// return `Please enter your email address.`
-// else return `` // an empty string
-const message = getEmailError(emailInput);
+// // check the user email input against all validation.
+// // if emailInput is blank
+// // return `Please enter your email address.`
+// // else return `` // an empty string
+// const message = getEmailError(emailInput);
 
-// if there is an email error e.g. if message !== blank
-showErrorValidation("#email-error", message);
-// pass the email error id and the message from getEmailError
+// // if there is an email error e.g. if message !== blank
+// showErrorValidation("#email-error", message);
+// // pass the email error id and the message from getEmailError
 
-function showErrorValidation(id, message) {
-   // use jQuery to toggle the error message and error styling
-}
+// function showErrorValidation(id, message) {
+//    // use jQuery to toggle the error message and error styling
+// }
