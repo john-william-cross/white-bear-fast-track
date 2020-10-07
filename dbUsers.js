0018600a91a5305998e023f1882ee7c5249efc00
John Cross <john.william.cross@gmail.com>
2020-10-07 10:24:34 -0700

    create dbUsers.js and copy that data from the link and put it into my file

---
+++ dbUsers.js
@@ -0,0 +1,30 @@
+const dbUsers = [
+   {
+      id: "003498",
+      email: "martin@gmail.com",
+      password: "D9729FEB74992CC3482B350163A1A010",
+      createdAt: 1601323030959,
+      isActive: true,
+   },
+   {
+      id: "112076",
+      email: "abigail@gmail.com",
+      password: "D58E3582AFA99040E27B92B13C8F2280",
+      createdAt: 1601323566304,
+      isActive: true,
+   },
+   {
+      id: "099001",
+      email: "dani@gmail.com",
+      password: "73A90ACAAE2B1CCC0E969709665BC62F",
+      createdAt: 1601323616825,
+      isActive: true,
+   },
+   {
+      id: "003498",
+      email: "martin@gmail.com",
+      password: "D9729FEB74992CC3482B350163A1A010",
+      createdAt: 1601323030959,
+      isActive: true,
+   },
+];
