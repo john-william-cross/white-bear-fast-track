296537659fef3ba0aaae3fabd80815e328928f41
John Cross <john.william.cross@gmail.com>
2020-10-08 07:25:37 -0700

    finish using regex to validate email; forgot to use .test :)

--- getEmailError.js
+++ getEmailError.js
@@ -1,9 +1,11 @@
 function getEmailError(email) {
    const emailPattern = /^[0-9a-zA-Z_].*@[a-zA-Z0-9]{1}\w+-*x?.[a-zA-Z]{2,}$/;
 
+   const isValidEmail = emailPattern.test(email);
+
    if (email === ``) {
       return `Please enter your email address.`;
-   } else if (email !== emailPattern) {
+   } else if (isValidEmail === false) {
       return `Please enter a valid email address.`;
    } else {
       return ``;
