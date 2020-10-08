3ccdfab51bd17d26c272d661deae834209b51a94
John Cross <john.william.cross@gmail.com>
2020-10-07 21:09:45 -0700

    begin validating email with regex

--- getEmailError.js
+++ getEmailError.js
@@ -1,6 +1,10 @@
 function getEmailError(email) {
+   const emailPattern = /^[0-9a-zA-Z_].*@[a-zA-Z0-9]{1}\w+-*x?.[a-zA-Z]{2,}$/;
+
    if (email === ``) {
       return `Please enter your email address.`;
+   } else if (email !== emailPattern) {
+      return `Please enter a valid email address.`;
    } else {
       return ``;
    }
